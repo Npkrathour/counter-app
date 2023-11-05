c99fceab0efe8bce536c2f4b427f8d093111029e
PINKU RATHOUR <95443990+Npkrathour@users.noreply.github.com>
2023-11-05 22:34:18 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CountImg from "./assets/myImg.jpg";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSub = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{ height: "100vh" }}
+      className="container-fluid bg-secondary d-flex align-items-center justify-content-center"
+    >
+      <div className="p-5 bg-white rounded-3 text-center border-5 border border-success shadow-lg">
+        <div className="counter-img">
+          <img
+            width={"100px"}
+            className="rounded-circle"
+            src={CountImg}
+            alt="Count"
+          />
+        </div>
+        <h2 className="fs-3 fw-bold mb-5">My Counter App</h2>
+        <p className="display-4 fw-bold">{count}</p>
+        <button
+          className="btn btn-success py-2 px-5 me-5 rounded fw-bold"
+          onClick={handleAdd}
         >
-          Learn React
-        </a>
-      </header>
+          Add +{" "}
+        </button>
+        <button
+          disabled={count === 0 && true}
+          className="btn btn-primary py-2 px-5 me-5 rounded fw-bold"
+          onClick={handleSub}
+        >
+          Sub -{" "}
+        </button>
+        <button
+          disabled={count === 0 ? true : false}
+          className="btn btn-danger py-2 px-5 rounded fw-bold"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
